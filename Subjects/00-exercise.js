0d3aa5bc20812ef77044ff63ac80004d872d178d
eshak almhtrm <ashak.alkateb@gmail.com>
2026-03-14 00:28:02 +0300

    update the calculator project1

---
+++ Subjects/00-exercise.js
@@ -0,0 +1,18 @@
+function toggleButton(selector){
+    let button = document.querySelector(selector);
+    if(button.classList.contains('is-toggled')){
+
+        button.classList.remove('is-toggled');
+
+    }
+    else{
+        turnOffPre();
+        button.classList.add('is-toggled');
+    }
+}
+function turnOffPre(){
+    const preButton = document.querySelector('.is-toggled');
+
+    if(preButton)
+        preButton.classList.remove('is-toggled');
+}
